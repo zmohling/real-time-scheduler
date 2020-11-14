1c1d5b4d834f8a7f50a97e5fe07538a5a96ae937
micdavis <michaeldavis.se@gmail.com>
2020-11-14 13:25:49 -0600

    exact anaylsis is working

--- src/scheduler.h
+++ src/scheduler.h
@@ -2,6 +2,9 @@
 #define SCHEDULER_H_
 
 #include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
 
 typedef struct task {
   uint8_t comp_time;
@@ -9,7 +12,26 @@ typedef struct task {
   uint8_t deadline;
 } task_t;
 
+int p_comparator(const void *p, const void *q);
+
+int d_comparator(const void *p, const void *q);
+
+int exact_analysis(task_t **tasks, uint8_t num_tasks, uint8_t is_dms);
+
+uint32_t gcd(int a, int b); 
+
+uint32_t find_lcm(task_t **tasks, uint8_t num_tasks);
+
 int rms(task_t **tasks, uint8_t num_tasks, task_t **schedule,
+        uint32_t *schedule_len);
+
+int dms(task_t **tasks, uint8_t num_tasks, task_t **schedule,
+        uint32_t *schedule_len);
+
+int edf(task_t **tasks, uint8_t num_tasks, task_t **schedule,
+        uint32_t *schedule_len);
+
+int llf(task_t **tasks, uint8_t num_tasks, task_t **schedule,
         uint32_t *schedule_len);
 
 #endif
